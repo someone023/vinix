5916b842672ecc86ef62d52123845837a56def7f
Ali Erkol <a.erkol@tu-braunschweig.de>
2024-01-27 12:03:09 +0100

    refactor

--- flake.nix
+++ flake.nix
@@ -1,52 +1,41 @@
 {
-  description = "A nixvim configuration";
+  description = "vinix";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixvim.url = "github:nix-community/nixvim";
-    flake-parts.url = "github:hercules-ci/flake-parts";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = {
+    self,
+    nixpkgs,
     nixvim,
-    flake-parts,
+    flake-utils,
     ...
   } @ inputs: let
-    config = import ./config; # import the module directly
+    config = import ./config;
   in
-    flake-parts.lib.mkFlake {inherit inputs;} {
-      systems = [
-        "x86_64-linux"
-      ];
-
-      perSystem = {
-        pkgs,
-        system,
-        ...
-      }: let
-        nixvimLib = nixvim.lib.${system};
-        nixvim' = nixvim.legacyPackages.${system};
-        nvim = nixvim'.makeNixvimWithModule {
-          inherit pkgs;
-          module = config;
-          # You can use `extraSpecialArgs` to pass additional arguments to your module files
-          extraSpecialArgs = {
-            # inherit (inputs) foo;
-          };
-        };
-      in {
-        checks = {
-          # Run `nix flake check .` to verify that your config is not broken
-          default = nixvimLib.check.mkTestDerivationFromNvim {
-            inherit nvim;
-            name = "A nixvim configuration";
-          };
-        };
-
-        packages = {
-          # Lets you run `nix run .` to start nixvim
-          default = nvim;
+    flake-utils.lib.eachDefaultSystem (system: let
+      nixvimLib = nixvim.lib.${system};
+      pkgs = import nixpkgs {inherit system;};
+      nixvim' = nixvim.legacyPackages.${system};
+      nvim = nixvim'.makeNixvimWithModule {
+        inherit pkgs;
+        module = config;
+        extraSpecialArgs = {
+          inherit self;
         };
       };
-    };
+    in {
+      checks = {
+        default = nixvimLib.check.mkTestDerivationFromNvim {
+          inherit nvim;
+          name = "vinix";
+        };
+      };
+
+      packages = {
+        default = nvim;
+      };
+    });
 }
